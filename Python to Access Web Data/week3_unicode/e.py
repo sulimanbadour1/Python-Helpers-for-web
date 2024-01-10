6ede2149ba93d14b8c1ee636978e2e8b993079b1
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2024-01-10 23:20:23 +0100

    added databases code

--- Python to Access Web Data/week3_unicode/e.py
+++ Python to Access Web Data/week3_unicode/e.py
@@ -1,15 +1,15 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 
-# # url = input("Enter - ")
+# url = input("Enter - ")
 # html = urllib.request.urlopen(url).read()
 # soup = BeautifulSoup(html, "html.parser")
-# # Retrieve all of the anchor tags
+# Retrieve all of the anchor tags
 # tags = soup("a")
 # count = 0
 # sum = 0
 # for tag in tags:
-#     # Look at the parts of a tag
+#     Look at the parts of a tag
 #     print(tag.get("href", None))
 
 
