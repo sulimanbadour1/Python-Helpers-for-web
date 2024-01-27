6fc483169fafda98a9d50e71ffdb4024f49be160
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2024-01-27 05:05:48 +0100

    Added d3 data

---
+++ Capstone/part2/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['year','umich.edu','ucdavis.edu','berkeley.edu','indiana.edu','ufp.pt','uct.ac.za','columbia.edu','etudes.org','gmail.com','virginia.edu'],
+['2005',57,11,12,12,10,14,13,5,10,9],
+['2006',116,37,34,33,32,28,28,30,23,22]
+];
